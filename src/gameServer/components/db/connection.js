d8e05cbb03af211963101d6f80430e1d27b2b53c
JongWoo Park <jjongwoo.dev@gmail.com>
2021-07-19 11:23:39 +0900

    feat(game-server): Change connection to use pool

--- src/gameServer/components/db/connection.js
+++ src/gameServer/components/db/connection.js
@@ -1,13 +1,14 @@
 import mysql from 'mysql2';
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: '15.164.202.251',
   user: 'goapp',
   password: 'Yx8kzu5CiWoU?1ABM',
-  database: 'town_dev'
+  database: 'town_dev',
+  connectionLimit: 4, 
 });
 
 //TODO: Transaction 처리로 안정성 올리고 pool을 이용한 connection 관리로 성능향상시키기.
 //아마 실시간으로 통신하는 serverEngine에 연결되어 발동되기때문에 lightweight query가 많지 않을까...생각합니다.
 
-export default connection;
+export default pool;
--- src/gameServer/components/room/roomRepository.js
+++ src/gameServer/components/room/roomRepository.js
@@ -1,6 +1,7 @@
-import connection from "../db/connection";
-import {db} from "../../../server/constants";
-import {Room} from "./room";
+import pool from "../db/connection";
+import { db } from "../../../server/constants";
+import { Room } from "./room";
+import { logger } from "../utils/logger";
 
 export class FirebaseRoom {
   constructor() {
@@ -10,123 +11,142 @@ export class FirebaseRoom {
   async getRoom(roomName) {
     const doc = await db.collection("rooms").doc(roomName).get();
     if (!doc.exists) {
-      throw new Error('Room does not exist in db');
+      throw new Error("Room does not exist in db");
     }
     return doc.data();
   }
 
   async getUserWithRoom(roomName, uid) {
-    const doc = await db.collection("rooms").doc(roomName).collection("users").doc(uid).get()
+    const doc = await db
+      .collection("rooms")
+      .doc(roomName)
+      .collection("users")
+      .doc(uid)
+      .get();
     if (!doc.exists) {
-      throw new Error('Record does not exist in db')
+      throw new Error("Record does not exist in db");
     }
     return doc.data();
   }
 
   updateRoom(roomName, data) {
-    return this.db.collection("rooms").doc(roomName).update(data)
+    return this.db.collection("rooms").doc(roomName).update(data);
   }
 }
 
 export class RoomRepository {
   constructor() {
-    this.conn = connection;
+    this.pool = pool;
   }
 
   async getRoom(rawRoomName) {
     return new Promise((resolve, reject) => {
-      const roomUrl = rawRoomName.substring(0, rawRoomName.indexOf('\\'));
-      const roomName = rawRoomName.substring(rawRoomName.indexOf('\\') + 1);
-      this.conn.query(
-        'SELECT * FROM `room` WHERE name = ? and room_url = ?',
-        [roomName, roomUrl],
-        (err, results) => {
-          if (err) reject(err);
-          else if (results.length === 0) reject("NOT FOUND")
-          else {
-            const ret = {
-              id: results[0].id,
-              name: results[0].name,
-              map: results[0].preset_id,
-              adminId: results[0].admin_id,
-              creatorId: results[0].creator_id,
-              password: results[0].password,
-              status: results[0].status,
-              purposeId: results[0].purpose_id,
-              roomUrl: results[0].room_url,
-              description: results[0].description,
-            };
-            resolve(new Room(rawRoomName, ret));
+      const roomUrl = rawRoomName.substring(0, rawRoomName.indexOf("\\"));
+      const roomName = rawRoomName.substring(rawRoomName.indexOf("\\") + 1);
+      this.pool.getConnection(function (err, conn) {
+        // Do something with the connection
+        conn.query(
+          "SELECT * FROM `room` WHERE name = ? and room_url = ?",
+          [roomName, roomUrl],
+          (err, results) => {
+            if (err) reject(err);
+            else if (results.length === 0) reject("NOT FOUND");
+            else {
+              const ret = {
+                id: results[0].id,
+                name: results[0].name,
+                map: results[0].preset_id,
+                adminId: results[0].admin_id,
+                creatorId: results[0].creator_id,
+                password: results[0].password,
+                status: results[0].status,
+                purposeId: results[0].purpose_id,
+                roomUrl: results[0].room_url,
+                description: results[0].description,
+              };
+              resolve(new Room(rawRoomName, ret));
+            }
           }
-        }
-      )
-    })
+        );
+        // Don't forget to release the connection when finished!
+        pool.releaseConnection(conn);
+      });
+    });
   }
 
   async getRoomWithUsers(rawRoomName) {
     return new Promise((resolve, reject) => {
-      const roomUrl = rawRoomName.substring(0, rawRoomName.indexOf('\\'));
-      const roomName = rawRoomName.substring(rawRoomName.indexOf('\\') + 1);
-      this.conn.query(
-        'SELECT *, ru.status as user_status FROM `room` r JOIN `room_user` ru ON r.id = ru.room_id WHERE r.name = ? and r.room_url = ?',
-        [roomName, roomUrl],
-        (err, results) => {
-          results[0].purpose_id = undefined;
-          if (err) reject(err);
-          else if (results.length === 0) reject("NOT FOUND")
-          else {
-            const ret = {
-              id: results[0].id,
-              name: results[0].name,
-              map: results[0].preset_id,
-              adminId: results[0].admin_id,
-              creatorId: results[0].creator_id,
-              password: results[0].password,
-              status: results[0].status,
-              purposeId: results[0].purpose_id,
-              roomUrl: results[0].room_url,
-              description: results[0].description,
-              bannedIPs: results.reduce((acc, cur) => {
-                acc[cur.user_id] = cur.user_status;
-                return acc;
-              }, {})
-            };
-            resolve(new Room(rawRoomName, ret));
+      const roomUrl = rawRoomName.substring(0, rawRoomName.indexOf("\\"));
+      const roomName = rawRoomName.substring(rawRoomName.indexOf("\\") + 1);
+
+      this.pool.getConnection(function (err, conn) {
+        // Do something with the connection
+        conn.query(
+          "SELECT *,r.status as room_status, ru.status as user_status FROM `room` r JOIN `room_user` ru ON r.id = ru.room_id WHERE r.name = ? and r.room_url = ?",
+          [roomName, roomUrl],
+          (err, results) => {
+            if (err) reject(err);
+            else {
+              logger.silly(JSON.stringify(results[0]));
+              const ret = {
+                id: results[0].id,
+                name: results[0].name,
+                map: results[0].preset_id,
+                adminId: results[0].admin_id,
+                creatorId: results[0].creator_id,
+                password: results[0].password,
+                status: results[0].room_status,
+                purposeId: results[0].purpose_id,
+                roomUrl: results[0].room_url,
+                description: results[0].description,
+                bannedIPs: results.reduce((acc, cur) => {
+                  acc[cur.user_id] = cur.user_status;
+                  return acc;
+                }, {}),
+              };
+              resolve(new Room(rawRoomName, ret));
+            }
           }
-        }
-      )
-    })
+        );
+        pool.releaseConnection(conn);
+      });
+    });
   }
 
   async updateRoom(roomName, data) {
     return new Promise((resolve, reject) => {
-      reject('Not Implemented');
-    })
+      reject("Not Implemented");
+    });
   }
 
   async updateRoomUser(roomFirebase, userId, status) {
     return new Promise((resolve, reject) => {
       const [roomUrl, roomName] = this._extractRoomUrlAndName(roomFirebase);
       // roomUrl과 roomName의 조합은 unique한 room_id를 식별해내는데 충분. 왜냐하면 roomUrl이 그걸 위해 존재하기 때문
-      this.conn.query(
-        'UPDATE `room_user` SET status=? WHERE user_id=? and room_id=(SELECT r.id FROM room r WHERE r.name = ? and r.room_url = ?)',
-        [status, userId, roomName, roomUrl],
-        (err, results) => {
-          if (err) reject(err);
-          else {
-            resolve(results.affectedRows > 0);
+      this.pool.getConnection(function (err, conn) {
+        // Do something with the connection
+        conn.query(
+          "UPDATE `room_user` SET status=? WHERE user_id=? and room_id=(SELECT r.id FROM room r WHERE r.name = ? and r.room_url = ?)",
+          [status, userId, roomName, roomUrl],
+          (err, results) => {
+            if (err) reject(err);
+            else {
+              resolve(results.affectedRows > 0);
+            }
           }
-        }
-      )
-    })
+        );
+        pool.releaseConnection(conn);
+      });
+    });
   }
 
   updateRoomStatus(roomFirebase, status) {
     return new Promise((resolve, reject) => {
       const [roomUrl, roomName] = this._extractRoomUrlAndName(roomFirebase);
       // roomUrl과 roomName의 조합은 unique한 room_id를 식별해내는데 충분. 왜냐하면 roomUrl이 그걸 위해 존재하기 때문
-      this.conn.query(
-        'UPDATE room SET status = ? WHERE name = ? and room_url = ?',
+      this.pool.getConnection(function (err, conn) {
+      conn.query(
+        "UPDATE room SET status = ? WHERE name = ? and room_url = ?",
         [status, roomName, roomUrl],
         (err, results) => {
           if (err) reject(err);
@@ -134,29 +154,34 @@ export class RoomRepository {
             resolve(results.affectedRows > 0);
           }
         }
-      )
-    })
+      );
+        pool.releaseConnection(conn);
+      });
+    });
   }
 
   _extractRoomUrlAndName(roomFirebase) {
-    const roomUrl = roomFirebase.substring(0, roomFirebase.indexOf('\\'));
-    const roomName = roomFirebase.substring(roomFirebase.indexOf('\\') + 1);
+    const roomUrl = roomFirebase.substring(0, roomFirebase.indexOf("\\"));
+    const roomName = roomFirebase.substring(roomFirebase.indexOf("\\") + 1);
     return [roomUrl, roomName];
   }
 
   updateRoomPassword(roomFirebase, password) {
     return new Promise((resolve, reject) => {
       const [roomUrl, roomName] = this._extractRoomUrlAndName(roomFirebase);
-      this.conn.query(
-        'UPDATE room SET password = ? WHERE name = ? and room_url = ?',
-        [password, roomName, roomUrl],
-        (err, results) => {
-          if (err) reject(err);
-          else {
-            resolve(results.affectedRows > 0);
+      this.pool.getConnection(function (err, conn) {
+        conn.query(
+          "UPDATE room SET password = ? WHERE name = ? and room_url = ?",
+          [password, roomName, roomUrl],
+          (err, results) => {
+            if (err) reject(err);
+            else {
+              resolve(results.affectedRows > 0);
+            }
           }
-        }
-      )
-    })
+        );
+        pool.releaseConnection(conn);
+      });
+    });
   }
 }
