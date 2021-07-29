5d990a05aec19ae84e5d95bbf7907276bc2a4966
taehyunkim <polepole0733@tenuto.co.kr>
2021-07-29 17:06:04 +0900

    homepage 새로운 api 적용

--- src/client/components/Homepage.jsx
+++ src/client/components/Homepage.jsx
@@ -1,118 +1,15 @@
-import React, {useEffect, useState} from 'react';
-import YesNoPrompt from './YesNoPrompt.jsx';
-import Feedback from './Feedback.jsx';
-import {localPreferences} from '../LocalPreferences.js';
-import {updateUserData} from '../userData.js';
-import {loginBackground, kakaoIcon, googleIcon, facebookIcon} from "../resources/images";
+import React, {useState} from 'react';
+import {facebookIcon, googleIcon, kakaoIcon, loginBackground} from "../resources/images";
 import './Homepage.css';
 import Modal from "react-modal";
-import GetServerData from "../api/GetServerData";
-
-import {RtToken} from "../lib/Utils";
 import {useHistory} from "react-router-dom";
-import {Config} from "../lib/Utils";
-import jwt_decode from "jwt-decode";
 import api from "../api/api";
 
-const Twitter = '/images/site/twitter.png';
+import CentiToken from "../api/CentiToken";
+import {useDispatch, useSelector} from "react-redux";
+import {setUserId} from "../redux/features/account/accountSlice";
+
 const {Kakao} = window;
-
-const CreateRoom = (props) => {
-  let [clicked, setClicked] = useState("");
-  let [user, setUser] = useState(localPreferences.get("user"))
-
-  let yesNoMessage = <div id="yes-no-message">
-                        <p>Are you above the age of 18?
-                          <span style={{fontWeight: "600"}}>
-                            (required)
-                          </span>
-                        </p>
-                      </div>;
-
-  useEffect(() => {
-    let handle = localPreferences.on("user", (info) => {
-      setUser(user);
-    })
-    return () => {
-      localPreferences.remove("user", handle);
-    };
-  }, [])
-
-  let dialog = <div></div>;
-
-  if (clicked === "") {
-    dialog = (
-      <div style={{
-          backgroundColor: "#0EBF55",
-          borderRadius: "10px",
-          width: "130px",
-          height: "34px",
-          margin: "20px auto 20px",
-          cursor: "pointer",
-        }}
-        onClick={() => {
-          if (user.overAge) {
-            props.onPrivate();
-          } else {
-            setClicked("private")
-          }
-        }}
-        >
-        <p className="button-text">CREATE ROOM</p>
-      </div>
-    );
-  } else if (!user.overAge) {
-    dialog = (
-      <YesNoPrompt
-        prompt={yesNoMessage}
-        onYes={() => {
-          updateUserData({ "overAge": true });
-          props.onPrivate();
-          setClicked("")
-        }}
-        onNo={() => { setClicked("") }}
-      />
-    );
-  }
-  return dialog;
-}
-
-function FeedbackLeft() {
-  const [showFeedback, setShowFeedback] = useState(false);
-
-  return (
-    <div style={{position: "absolute", width: "100%", top: "40px", textAlign: "center"}}>
-      <div style={{width: "600px", textAlign: "left", margin: "auto"}}>
-        <div className="ot-header-feedback">
-        <div className="action" onClick={() => {setShowFeedback(!showFeedback)}}>Feedback</div>
-          { showFeedback ?
-            <Feedback onCancel={() => {setShowFeedback(false)}}/>
-            :
-            <div></div>
-          }
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function Top() {
-
-  let createRoom = (
-    <CreateRoom
-      onPrivate={() => {
-        window.location.href = "/private"
-      }}
-    />
-  );
-
-  return (
-    <div style={{maxWidth: "338px", textAlign: "center", marginTop: "100px"}} className="writing">
-      {createRoom}
-    </div>
-
-  );
-}
 
 function LoginWithKakao({login}) {
 
@@ -269,65 +166,29 @@ function Logout({logout}) {
 }
 
 export default function Homepage() {
-  const [isLogin, setLogin] = useState(undefined);
   const [modalIsOpen, setIsOpen] = useState(false);
   const history = useHistory();
+  const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (!localStorage.getItem('access_token')) {
-      setLogin(false);
-    } else {
-      setLogin(true);
-      Kakao.API.request({
-        url: '/v2/user/me'
-      }).then((res) => console.log('/v2/user/me', res));
-    }
-
-  }, [isLogin])
+  const userId = useSelector(({account: {userId}}) => userId)
 
   const login = () => {
     Kakao.Auth.login({
-      success: function (authObj) {
-        console.log('access_token, refresh_token : ', authObj.access_token, authObj.refresh_token);
-
-        const req = {
-          account_type: "KAKAO",
-          account_token: authObj.access_token
-        };
-
-        GetServerData(req, '/v1/user/login', (res) => {
-          RtToken.save(res.access_token, res.expires_at, res.refresh_token).then(() => {
-            const tokenInfo = jwt_decode(res.access_token);
-            console.log("TOKEN_INFO_LOGIN", tokenInfo);
-            Config.myUserId = tokenInfo.UserId;
-            history.push({pathname: '/createProfile'});
-          });
-
-        }, (e) => {
-          console.log("error:" + JSON.stringify(e))
-        })
-
-        api.login('KAKAO', authObj.access_token).then();
-
+      success: async function (authObj) {
+        const userId = await api.login('KAKAO', authObj.access_token);
+        dispatch(setUserId(userId));
+        history.push({pathname: '/createProfile'});
       },
-      fail: function (err) {
-        console.log(JSON.stringify(err))
+      fail: function (e) {
+        console.log(JSON.stringify(e))
       },
     })
   }
 
   const logout = () => {
-    if (!Kakao.Auth.getAccessToken()) {
-      console.log('Not logged in.');
-      return;
-    }
-
-    Kakao.Auth.logout(function () {
-      console.log(Kakao.Auth.getAccessToken());
-      localStorage.removeItem('access_token');
-      localStorage.removeItem('refresh_token');
-      document.location.href = '/';
-    });
+    CentiToken.remove();
+    Kakao.Auth.logout();
+    dispatch(setUserId(null));
   }
 
   function openModal() {
@@ -343,15 +204,31 @@ export default function Homepage() {
       <div style={{backgroundImage: `url(${loginBackground})`, height: "100%",  backgroundRepeat : "no-repeat", backgroundSize : "cover"}}>
         <div style={{display: "flex", justifyContent: "space-between", padding: "50px 50px 0 50px", alignItems: "center"}}>
           <div style={{fontSize: "60px", fontWeight: "bold", color: "white"}}>센티미터</div>
-
-          {Kakao.Auth.getAccessToken() ? <div><Logout logout={logout}/></div> : <div
-            onClick={() => {Kakao.Auth.getAccessToken() ? document.location.href = '/createProfile' : openModal()}}
-            style={{backgroundColor: "dodgerblue", width: "200px", height: "60px", borderRadius: "10px", color: "white", fontSize: "20px", display: "flex", alignItems: "center", justifyContent: "center"}}>
-            서비스 시작
-          </div>}
+          {
+            userId ?
+              <div>
+                <Logout logout={logout}/>
+              </div>
+              :
+              <div
+                onClick={openModal}
+                style={{
+                  backgroundColor: "dodgerblue",
+                  width: "200px",
+                  height: "60px",
+                  borderRadius: "10px",
+                  color: "white",
+                  fontSize: "20px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center"
+                }}>
+                서비스 시작
+              </div>
+          }
         </div>
-
-        {modalIsOpen &&
+        {
+          modalIsOpen &&
           <div>
             <Modal
               isOpen={modalIsOpen}
@@ -378,25 +255,28 @@ export default function Homepage() {
               contentLabel="Example Modal"
             >
               <div style={{marginTop: "56px"}}>
-                <h2 style={{color: "#939393", fontSize: "18px", display: "flex", justifyContent: "center"}}>Welcome To</h2>
+                <h2 style={{color: "#939393", fontSize: "18px", display: "flex", justifyContent: "center"}}>Welcome
+                  To</h2>
                 <h1 style={{color: "#FF6F61", fontSize: "36px"}}>CENTIMETER</h1>
               </div>
-              <div style={{width: "100%", display: "flex", alignItems: "center", justifyContent: "center", marginTop: "50px"}}>
-                <div style={{borderTop: "1px solid #E5E5E5", width: "100px"}} />
+              <div style={{
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                marginTop: "50px"
+              }}>
+                <div style={{borderTop: "1px solid #E5E5E5", width: "100px"}}/>
                 <div style={{color: "#AEAEAE", fontSize: "14px", margin: "0 5px", fontWeight: "normal"}}>아래 소셜계정으로 로그인</div>
-                <div style={{borderTop: "1px solid #E5E5E5", width: "100px"}} />
+                <div style={{borderTop: "1px solid #E5E5E5", width: "100px"}}/>
               </div>
-
               <div style={{marginTop: "25px", marginBottom: "40px"}}>
-                {!isLogin ?
-                  <div>
-                    <LoginWithGoogle  />
-                    <LoginWithKakao login={login} />
-                    <LoginWithFacebook  />
-                  </div> : <></>
-                }
+                <div>
+                  <LoginWithGoogle/>
+                  <LoginWithKakao login={login}/>
+                  <LoginWithFacebook/>
+                </div>
               </div>
-
             </Modal>
           </div>
         }
