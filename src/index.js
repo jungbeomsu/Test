80ff03c263f0fdbd44278d2207c9ce46e1a99144
jinhwa <jinhwa.jang@tenuto.co.kr>
2021-07-30 11:41:19 +0900

    .

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import {configureStore} from "@reduxjs/toolkit";
 import {Provider} from 'react-redux';
 import rootReducer from './client/redux/reducers';
 import serviceApi from "./client/api/service";
+import api from "./client/api/api";
 
 const store = configureStore({
   reducer: rootReducer,
 });
 
 serviceApi.setDispatch(store.dispatch);
+api.setDispatch(store.dispatch);
 
 ReactDOM.render(
     <Provider store={store}>
